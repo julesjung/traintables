cdff1c729c3c929510d91bc8e0330440c49d6e73
julesjung <94782485+julesjung@users.noreply.github.com>
2025-12-26 21:37:30 +0100

    fix: use pooler session mode for IPv4 compatibility

--- gtfs/src/trips.rs
+++ gtfs/src/trips.rs
@@ -12,8 +12,6 @@ pub struct GTFSTrip {
     pub service_id: u32,
     #[serde(rename = "trip_headsign")]
     pub headsign: String,
-    #[serde(rename = "direction_id")]
-    pub direction: Option<u8>,
 }
 
 #[derive(Serialize)]
@@ -22,7 +20,6 @@ pub struct Trip {
     pub route_id: String,
     pub service_id: u32,
     pub headsign: String,
-    pub direction: Option<u8>,
     pub origin_id: String,
     pub destination_id: String,
 }
@@ -34,7 +31,6 @@ impl Trip {
             route_id: gtfs_trip.route_id,
             service_id: gtfs_trip.service_id,
             headsign: gtfs_trip.headsign,
-            direction: gtfs_trip.direction,
             origin_id,
             destination_id,
         }
