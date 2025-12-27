8f2c302ef38ced85b2e85856dee30eee4155c077
julesjung <94782485+julesjung@users.noreply.github.com>
2025-12-27 13:02:26 +0100

    fix: change time type

--- gtfs/src/stop_times.rs
+++ gtfs/src/stop_times.rs
@@ -1,13 +1,20 @@
-use anyhow::Result;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de};
 use std::{collections::HashMap, io::Cursor};
 
 #[derive(Deserialize, Serialize)]
 pub struct StopTime {
     pub trip_id: String,
     pub stop_id: String,
-    pub arrival_time: String,
-    pub departure_time: String,
+    #[serde(
+        deserialize_with = "deserialize_time",
+        rename(deserialize = "arrival_time")
+    )]
+    pub arrival_seconds: u32,
+    #[serde(
+        deserialize_with = "deserialize_time",
+        rename(deserialize = "departure_time")
+    )]
+    pub departure_seconds: u32,
     pub stop_sequence: u8,
 }
 
@@ -25,7 +32,22 @@ impl Endpoints {
     }
 }
 
-pub fn parse_stop_times(data: &Vec<u8>) -> Result<(Vec<StopTime>, HashMap<String, Endpoints>)> {
+fn deserialize_time<'de, D>(deserializer: D) -> Result<u32, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    let time: &str = Deserialize::deserialize(deserializer).unwrap();
+    let mut time = time.split(":");
+    let hours: u32 = time.next().unwrap().parse().map_err(de::Error::custom)?;
+    let minutes: u32 = time.next().unwrap().parse().map_err(de::Error::custom)?;
+    let seconds: u32 = time.next().unwrap().parse().map_err(de::Error::custom)?;
+
+    Ok(hours * 3600 + minutes * 60 + seconds)
+}
+
+pub fn parse_stop_times(
+    data: &Vec<u8>,
+) -> anyhow::Result<(Vec<StopTime>, HashMap<String, Endpoints>)> {
     let cursor = Cursor::new(data);
     let mut reader = csv::Reader::from_reader(cursor);
 
@@ -46,7 +68,7 @@ pub fn parse_stop_times(data: &Vec<u8>) -> Result<(Vec<StopTime>, HashMap<String
     Ok((stop_times, endpoints))
 }
 
-pub fn generate_stop_times_csv(stop_times: Vec<StopTime>, path: &str) -> Result<()> {
+pub fn generate_stop_times_csv(stop_times: Vec<StopTime>, path: &str) -> anyhow::Result<()> {
     let mut writer = csv::Writer::from_path(path)?;
 
     for stop_time in stop_times {
