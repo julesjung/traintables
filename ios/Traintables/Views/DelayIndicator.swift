50ba138a26d242074309303f85329faf1e46a7d7
julesjung <94782485+julesjung@users.noreply.github.com>
2026-02-23 12:01:49 +0100

    add supabase migrations

---
+++ ios/Traintables/Views/DelayIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  DelayIndicator.swift
+//  Traintables
+//
+//  Created by Jules on 29/12/2025.
+//
+
+import SwiftUI
+
+struct DelayIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DelayIndicator()
+}
